b369a509b522b8f82e285f1ec976077c1e23051b
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2023-12-07 21:56:31 +0700

    update

--- src/utils/connect-cloudinary.ts
+++ src/utils/connect-cloudinary.ts
@@ -2,8 +2,8 @@ import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({ 
   cloud_name: 'dslnmqvlb', 
-  api_key: '549695228267364', 
-  api_secret: 'Q8ksXeJe_kNaVpAEeJLkjYkZobQ' 
+  api_key: `${process.env.CLOUDINARY_API_KEY}`, 
+  api_secret: `${process.env.CLOUDINARY_API_SECRET}` 
 });
 
 export default cloudinary;
